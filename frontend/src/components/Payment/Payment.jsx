cf176167ad568b3e1eac480486d0149fb88505ca
Bilal3940 <bilalj3940@gmail.com>
2024-05-03 01:55:37 +0500

    payment method

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -13,8 +13,22 @@ const Payment = () => {
   const [open, setOpen] = useState(false);
   const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  // const stripe = useStripe();
-  // const elements = useElements();
+  const [dropInInstance, setDropInInstance] = useState(null);
+
+
+  const initializeDropIn = () => {
+    // Initialize Braintree Drop-in
+    dropin.create({
+      authorization: 'sandbox_w3yzc2kz_3nz8xzxsrp3jdtbj',
+      container: '#dropin-container'
+    }, (createErr, instance) => {
+      if (createErr) {
+        console.error('Error initializing Drop-in:', createErr);
+        return;
+      }
+      setDropInInstance(instance);
+    });
+  };
 
   useEffect(() => {
     const orderData = JSON.parse(localStorage.getItem("latestOrder"));
@@ -50,98 +64,74 @@ const Payment = () => {
     totalPrice: orderData?.totalPrice,
   };
 
-  // const onApprove = async (data, actions) => {
-  //   return actions.order.capture().then(function (details) {
-  //     const { payer } = details;
-
-  //     let paymentInfo = payer;
-
-  //     if (paymentInfo !== undefined) {
-  //       paypalPaymentHandler(paymentInfo);
-  //     }
-  //   });
-  // };
-
-  // const paypalPaymentHandler = async (paymentInfo) => {
-  //   const config = {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-
-  //   order.paymentInfo = {
-  //     id: paymentInfo.payer_id,
-  //     status: "succeeded",
-  //     type: "Paypal",
-  //   };
-
-  //   await axios
-  //     .post(`${server}/order/create-order`, order, config)
-  //     .then((res) => {
-  //       setOpen(false);
-  //       navigate("/order/success");
-  //       toast.success("Order successful!");
-  //       localStorage.setItem("cartItems", JSON.stringify([]));
-  //       localStorage.setItem("latestOrder", JSON.stringify([]));
-  //       window.location.reload();
-  //     });
-  // };
-
   const paymentData = {
     amount: Math.round(orderData?.totalPrice * 100),
   };
 
-  // const paymentHandler = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const config = {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     };
-
-  //     const { data } = await axios.post(
-  //       `${server}/payment/process`,
-  //       paymentData,
-  //       config
-  //     );
-
-  //     const client_secret = data.client_secret;
-
-  //     if (!stripe || !elements) return;
-  //     const result = await stripe.confirmCardPayment(client_secret, {
-  //       payment_method: {
-  //         card: elements.getElement(CardNumberElement),
-  //       },
-  //     });
-
-  //     if (result.error) {
-  //       toast.error(result.error.message);
-  //     } else {
-  //       if (result.paymentIntent.status === "succeeded") {
-  //         order.paymnentInfo = {
-  //           id: result.paymentIntent.id,
-  //           status: result.paymentIntent.status,
-  //           type: "Credit Card",
-  //         };
-
-  //         await axios
-  //           .post(`${server}/order/create-order`, order, config)
-  //           .then((res) => {
-  //             setOpen(false);
-  //             navigate("/order/success");
-  //             toast.success("Order successful!");
-  //             localStorage.setItem("cartItems", JSON.stringify([]));
-  //             localStorage.setItem("latestOrder", JSON.stringify([]));
-  //             window.location.reload();
-  //           });
-  //       }
-  //     }
-  //   } catch (error) {
-  //     toast.error(error);
-  //   }
-  // };
-
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (dropInInstance) {
+        dropInInstance.requestPaymentMethod(async (requestPaymentMethodErr, payload) => {
+          if (requestPaymentMethodErr) {
+            console.error('Error requesting payment method:', requestPaymentMethodErr);
+            return;
+          }
+          // Send payment method nonce to server
+          try {
+            const response = await fetch(`${server}/api/v2/payment/checkout`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ paymentMethodNonce: payload.nonce })
+            });
+          
+            const result = await response.json();
+          
+            // Define payment information object
+            const paymentInfo = {
+              status: result.success ? 'succeeded' : 'failed',
+              type: 'Paypal' // Update this with the actual payment method used
+            };
+  
+            // Send order with payment information to server
+            const config = {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            };
+  
+            const orderData = { ...order, paymentInfo };
+  
+            await axios
+              .post(`${server}/api/v2/order/create-order`, orderData, config)
+              .then((res) => {
+                setOpen(false);
+                navigate("/order/success");
+                toast.success("Order successful!");
+                localStorage.setItem("cartItems", JSON.stringify([]));
+                localStorage.setItem("latestOrder", JSON.stringify([]));
+              });
+  
+            // Handle success or failure
+            if (result.success) {
+              // Handle success
+              console.log('Success:', result);
+            } else {
+              // Handle failure
+              console.error('Error:', result);
+            }
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+  
   const cashOnDeliveryHandler = async (e) => {
     e.preventDefault();
     console.log("cash on dilevery")
@@ -174,12 +164,13 @@ const Payment = () => {
         <div className="w-full 800px:w-[65%]">
           <PaymentInfo
           // Opensandbox= {Opensandbox}
+          initializeDropIn={initializeDropIn}
             user={user}
             open={open}
             setOpen={setOpen}
             // onApprove={onApprove}
             createOrder={createOrder}
-            // paymentHandler={paymentHandler}
+            paymentHandler={paymentHandler}
             cashOnDeliveryHandler={cashOnDeliveryHandler}
           />
         </div>
@@ -195,72 +186,22 @@ const PaymentInfo = ({
   user,
   open,
   setOpen,
-  onApprove,
-  createOrder,
+
+  initializeDropIn,
+
   paymentHandler,
   cashOnDeliveryHandler,
 }) => {
 
   const [select, setSelect] = useState(1);
-  const [dropInInstance, setDropInInstance] = useState(null);
   const Opensandbox = () => {
-    setSelect(2); // Set select to 2 when "Pay Now" button is clicked
+    setSelect(2); 
     initializeDropIn();
   };
-
-  const initializeDropIn = () => {
-    // Initialize Braintree Drop-in
-    dropin.create({
-      authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
-      container: '#dropin-container'
-    }, (createErr, instance) => {
-      if (createErr) {
-        console.error('Error initializing Drop-in:', createErr);
-        return;
-      }
-      setDropInInstance(instance);
-    });
-  };
-  const handlePayment = () => {
-    if (dropInInstance) {
-      dropInInstance.requestPaymentMethod((requestPaymentMethodErr, payload) => {
-        if (requestPaymentMethodErr) {
-          console.error('Error requesting payment method:', requestPaymentMethodErr);
-          return;
-        }
-        // Send payment method nonce to server
-        fetch(`${server}/api/v2/payment/checkout`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ paymentMethodNonce: payload.nonce })
-        })
-          .then(response => response.json())
-          .then(result => {
-            if (result.success) {
-              // Handle success
-              console.log('Success:', result);
-            } else {
-              // Handle error
-              console.error('Error:', result);
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-      });
-    }
-  };
-
   return (
     <div className="w-full 800px:w-[95%] bg-[#fff] rounded-md p-5 pb-8">
       {/* select buttons */}
-      <div>
 
-      </div>
-
-      <br />
       {/* card payment */}
       <div>
         <div className="flex w-full pb-5 border-b mb-2">
@@ -281,7 +222,7 @@ const PaymentInfo = ({
         {select === 2 ? (
           <div className="w-full flex-column border-b">
       <div id="dropin-container"></div>
-      <button  className={`${styles.button} !bg-[#f63b60] text-white h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}  onClick={handlePayment}>Confirm</button>
+      <button  className={`${styles.button} !bg-[#f63b60] text-white h-[45px] rounded-[5px] cursor-pointer text-[18px] font-[600]`}  onClick={paymentHandler}>Confirm</button>
           </div>
         ) : null}
 
